37c2e1243b0b56966c2a09cc66377d02112f61a2
davidoso2023 <davida-osorio@unilibre.edu.co>
2025-08-06 21:29:44 -0500

    tonterias

--- proyectox/SistemaPos/urls.py
+++ proyectox/SistemaPos/urls.py
@@ -1,9 +1,8 @@
-
 from django.urls import path
-from .views import home_view
+from .views import home_view, Mesero1
 
 
 urlpatterns = [
-
-    path('home/', home_view, name='home'),
+    path("home/", home_view, name="home"),
+    path("Mesero1/", Mesero1, name="Mesero"),
 ]
--- proyectox/SistemaPos/views.py
+++ proyectox/SistemaPos/views.py
@@ -2,6 +2,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
 @login_required
 def home_view(request):
-    return render(request, 'core/home.html')
+    return render(request, "core/home.html")
+
+
+def Mesero1(request):
+    return render(request, "core/MeseroInicial.html")
